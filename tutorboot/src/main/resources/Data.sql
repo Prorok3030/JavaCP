30a9ed5c3cdcd6631f56b7b4d7cddea9c218c874
Alexey <nice.isakin@mail.ru>
2022-09-28 18:45:07 +0300

    Restored

--- tutorboot/src/main/resources/Data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO difficulty (id, name, points) VALUES (1, 'low', 5);
-INSERT INTO difficulty (id, name, points) VALUES (2, 'medium', 10);
-INSERT INTO difficulty (id, name, points) VALUES (3, 'high', 20);
